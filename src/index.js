86664dbe6b85722ec613b1685dbb6a1950c1c3e2
AlanVH <alanvhz.09@gmail.com>
2021-05-05 11:05:47 -0500

    React project created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Variables y constantes
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/**
+ * En React ya no debemos usar 'var' para declarar 
+ * variables ni en el js moderno.
+ */
+
+const name = "Alan React Dev";
+let lastname = "Vargas";
+
+// Template Strings =====================
+
+const nombre = "Alan";
+const apellido = "Vargas"
+
+const fullname = nombre + apellido // Sin template string
+const fullnameTs = `${ nombre } ${ apellido }` // Con template string
+
+function getSecondLastname() {
+    return "Hernández"
+}
+
+const fullnameTs2 = `Mi nombre es ${ fullnameTs } ${ getSecondLastname() }`;
+console.log("Template String: ", fullnameTs2);
+
+// Objetos literales ===================
+
+const obj = {
+
+    llave: "valor",
+    nombre: "Alan",
+    edad: 24,
+
+}
+
+const copy = { ...obj } // Sintaxis Spread
+copy.nombre = "Juan";
+
+console.log("ObjLit: ", obj);
+console.log("ObjLitCopia: ", copy);
+
+/**
+ * La sinstaxis spread se ocupa para crear una copia de un objeto, en este caso
+ * del objeto 'obj', pues si lo hiciéramos con una igualacion no tendríamos
+ * una copia, sino una referencia en memoria por lo que afectaríamos también al obj
+ * cuando modifiquemos 'copy'. Ej:
+ * 
+ * const copy = obj; <- Esto está mal
+ */
+
+// Arreglos =====================
+
+const arr = [1,2,3,4];
+let arr2 = [...arr, 5]; // Spread también se puede ocupar para copiar arreglos.
+const arr3 = arr2.map( function(x) {
+    return x * 2
+})
+
+console.log("arr: ", arr);
+console.log("arr2: ", arr2);
+console.log("arr3: ", arr3);
+
+// Funciones ======================
+
+// Usamos const para declarar una función
+const myFunc = function(name) {
+    return `Hola ${ name }`;
+}
+
+// ||
+
+const arrowFunc = ( name ) => `Hola ${ name }`;
+
+
